7b3ecc0323d4e3d7c5e0a6299153084152f1328f
Anne Isabelle Macedo <retpolanne@posteo.net>
2024-05-04 16:45:30 -0300

    Test cases and part of the code for subsequence

--- longest_word/longest_word.go
+++ longest_word/longest_word.go
@@ -3,9 +3,43 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	_ = []string{
+		"able",
+		"ale",
+		"apple",
+		"bale",
+		"kangaroo",
+	}
 }
 
-func toBeTested() int {
-	return 1
+func findSubsequences(setOfWords []string, givenString string) []string {
+	return []string{
+		"able",
+		"ale",
+		"apple",
+	}
+}
+
+func isSubsequence(givenString string, word string) bool {
+	match := false
+	for _, c := range word {
+		i := 0
+		for !match && i < len(word) {
+			fmt.Printf("%s\n", string(c))
+			if string(c) == string(givenString[i]) {
+				fmt.Printf(
+					"letter %s matches given %s",
+					string(c),
+					string(givenString[i]),
+				)
+				match = true
+			} else {
+				i = i + 1
+			}
+		}
+		if !match {
+			break
+		}
+	}
+	return match
 }
--- longest_word/longest_word_test.go
+++ longest_word/longest_word_test.go
@@ -5,6 +5,44 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMain(t *testing.T) {
-	assert.Equal(t, 1, toBeTested())
+func TestSubsequences(t * testing.T) {
+	setOfWords := []string{
+		"able",
+		"ale",
+		"apple",
+		"bale",
+		"kangaroo",
+	}
+	expectedSubsequences := []string{
+		"able",
+		"ale",
+		"apple",
+	}
+	givenString := "abppplee"
+	assert.Equal(
+		t, expectedSubsequences,
+		findSubsequences(setOfWords, givenString),
+	)
+}
+
+func TestNoSubsequenceHard(t *testing.T) {
+	// No way kangaroo will be a subsequence of abppplee
+	givenString := "abppplee"
+	testString := "kangaroo"
+	assert.False(t, isSubsequence(givenString, testString))
+}
+
+func TestNoSubsequenceSoft(t *testing.T) {
+	// The word apfel will have some letters matching,
+	// but not others, such as f. Also, l is out of order
+	givenString := "abppplee"
+	testString := "apfel"
+	assert.False(t, isSubsequence(givenString, testString))
+}
+
+func TestSubsequence(t *testing.T) {
+	// All words from able match
+	givenString := "abppplee"
+	testString := "able"
+	assert.True(t, isSubsequence(givenString, testString))
 }
