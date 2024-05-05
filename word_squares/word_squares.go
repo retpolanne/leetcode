d207a06fb733ab8532292e94cf196c4cd2f59b38
Anne Isabelle Macedo <retpolanne@posteo.net>
2024-05-04 21:12:48 -0300

    Test coverage and new functions

--- word_squares/word_squares.go
+++ word_squares/word_squares.go
@@ -3,6 +3,7 @@ package main
 import (
 	"strings"
 	"fmt"
+	"slices"
 )
 
 func main() {
@@ -13,21 +14,23 @@ func isWordSquare(arrayOfWords []string) bool {
 	// Assume it's a word square default
 	// we'll prove it's not!
 	wordSquare := true
-	wordLen := len(arrayOfWords[0])
-	for i, word := range arrayOfWords {
-		if (wordLen != len(word)) {
-			wordSquare = false
-			break
-		}
-
-		if (wordLen != len(arrayOfWords)) {
-			wordSquare = false
-			break
-		} else {
-			// It's a square, now let's check the words
-			wordSquare = wordSameVertically(i, arrayOfWords)
-			if !wordSquare {
+	if (len(arrayOfWords) > 0) {
+		wordLen := len(arrayOfWords[0])
+		for i, word := range arrayOfWords {
+			if (wordLen != len(word)) {
+				wordSquare = false
 				break
+			}
+
+			if (wordLen != len(arrayOfWords)) {
+				wordSquare = false
+				break
+			} else {
+				// It's a square, now let's check the words
+				wordSquare = wordSameVertically(i, arrayOfWords)
+				if !wordSquare {
+					break
+				}
 			}
 		}
 	}
@@ -40,9 +43,77 @@ func wordSameVertically(index int, arrayOfWords []string) bool {
 		sb.WriteString(string(word[index]))
 	}
 
-	fmt.Printf("Is %s == %s\n", sb.String(), arrayOfWords[index])
 	if sb.String() == arrayOfWords[index] {
 		return true
 	}
 	return false
+}
+
+func separateWordsWithSameLength(arrayOfWords []string) map[int][]string {
+	wordLengths := map[int][]string{}
+	for _, word := range arrayOfWords {
+		wordLengths[len(word)] = append(
+			wordLengths[len(word)],
+			word,
+		)
+	}
+	return wordLengths
+}
+
+func generatePossibleWordSquares(arrayOfWords []string) [][]string {
+	possibleSquares := [][]string{}
+	wordsWithSameLength := separateWordsWithSameLength(arrayOfWords)
+	for _, wordListSameLength := range wordsWithSameLength {
+		wordSquare := []string{}
+		for i, word := range wordListSameLength {
+			fmt.Println(word)
+			// Iterate through words with same length
+			if i > 0 {
+				// We don't care about the zeroth char
+				// we also don't care for the first word
+				// We start extracting the vertical from the i=1,
+				// which should be a single letter
+				verticalWord := extractVertical(wordSquare, i)
+				// If we didn't find a vertical word, then we should just
+				// go to the next word!
+				if verticalWord != "" {
+					// Found vertical word, let's find all words that
+					// start with this vertical word
+					possibleWords := findWordsThatStartsWith(verticalWord, wordListSameLength, wordSquare)
+					if len(possibleWords) > 0 {
+						// TODO
+						// we can have something like LEAD and LEED for example
+						wordSquare = append(wordSquare, possibleWords[0])
+					}
+				}
+			}
+		}
+
+		if (isWordSquare(wordSquare) && len(wordSquare) > 0) {
+			possibleSquares = append(possibleSquares, wordSquare)
+		}
+	}
+	return possibleSquares
+}
+
+func extractVertical(arrayOfWords []string, index int) string {
+	var sb strings.Builder
+	for _, word := range arrayOfWords {
+		sb.WriteString(string(word[index]))
+	}
+	return sb.String()
+}
+
+func findWordsThatStartsWith(start string, arrayOfWords []string, ignore []string) []string {
+	wordList := []string{}
+	for _, word := range arrayOfWords {
+		if len(word) < len(start) {
+			continue
+		}
+		wordBeginning := word[0 : len(start)]
+		if wordBeginning == start && !slices.Contains(ignore, word) {
+			wordList = append(wordList, word)
+		}
+	}
+	return wordList
 }
--- word_squares/word_squares_test.go
+++ word_squares/word_squares_test.go
@@ -71,3 +71,65 @@ func TestWordNotSameVertically(t *testing.T) {
 	words := []string{"BALL", "IREA", "LEAD", "LADY"}
 	assert.False(t, wordSameVertically(1, words))
 }
+
+func TestGeneratePossibleWordSquares(t *testing.T) {
+	words := []string{"AREA", "BALL", "DEAR", "LADY", "LEAD", "YARD"}
+	output := [][]string{
+		[]string{"BALL", "AREA", "LEAD", "LADY"},
+		[]string{"LADY", "AREA", "DEAR", "YARD"},
+	}
+	assert.Equal(t, generatePossibleWordSquares(words), output)
+}
+
+func TestGeneratePossibleWordSquaresPt2(t *testing.T) {
+	words := []string{"AREA", "BALL", "DEAR", "LADY", "LEAD", "YARD", "GATINHO"}
+	output := [][]string{
+		[]string{"BALL", "AREA", "LEAD", "LADY"},
+		[]string{"LADY", "AREA", "DEAR", "YARD"},
+	}
+	assert.Equal(t, output, generatePossibleWordSquares(words))
+}
+
+func TestGeneratePossibleWordSquaresPt3(t *testing.T) {
+	words := []string{"AREA", "BALL", "DEAR", "LADY", "LEAD", "LEED", "YARD", "AB", "BA"}
+	output := [][]string{
+		[]string{"BALL", "AREA", "LEAD", "LADY"},
+		[]string{"LADY", "AREA", "DEAR", "YARD"},
+		[]string{"AB", "BA"},
+	}
+	assert.Equal(t, output, generatePossibleWordSquares(words))
+}
+
+func TestSeparateWordsWithSameLength(t *testing.T) {
+	words := []string{"AREA", "BALL", "DEAR", "LADY", "LEAD", "YARD", "AB", "BA"}
+	output := map[int][]string{
+		4: []string{"AREA", "BALL", "DEAR", "LADY", "LEAD", "YARD"},
+		2: []string{"AB", "BA"},
+	}
+	assert.Equal(t, output, separateWordsWithSameLength(words))
+}
+
+func TestFindWordThatStartsEmpty(t *testing.T) {
+	words := []string{"AREA", "BALL", "DEAR", "LADY", "LEAD", "LEED", "YARD", "BA"}
+	assert.Equal(t, []string{}, findWordsThatStartsWith("A", words, []string{"AREA"}))
+}
+
+func TestFindWordThatStartsWith(t *testing.T) {
+	words := []string{"AREA", "BALL", "DEAR", "LADY", "LEAD", "LEED", "YARD", "AB", "BA"}
+	assert.Equal(t, []string{"LEAD", "LEED"}, findWordsThatStartsWith("LE", words, []string{}))
+}
+
+func TestFindWordThatStartsWithIgnore(t *testing.T) {
+	words := []string{"AREA", "BALL", "DEAR", "LADY", "LEAD", "LEED", "YARD", "AB", "BA"}
+	assert.Equal(t, []string{"LEAD"}, findWordsThatStartsWith("LE", words, []string{"LEED"}))
+}
+
+func TestExtractVertical(t *testing.T) {
+	words := []string{"BALL", "AREA"}
+	assert.Equal(t, "LE", extractVertical(words, 2))
+}
+
+func TestExtractVerticalSingleItem(t *testing.T) {
+	words := []string{"BALL"}
+	assert.Equal(t, "A", extractVertical(words, 1))
+}
