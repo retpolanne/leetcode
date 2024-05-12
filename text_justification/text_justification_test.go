0707d42e9394cb307eba9a5490aa76b8d00eb140
Anne Isabelle Macedo <retpolanne@posteo.net>
2024-05-12 14:13:50 -0300

    Added more test cases

--- text_justification/text_justification_test.go
+++ text_justification/text_justification_test.go
@@ -39,8 +39,19 @@ func TestLeftJustifyLastLine(t *testing.T) {
 	assert.Equal(t, exampleLine, leftJustify([]string{"defense", "mechanism"}, 20))
 }
 
+func TestLeftJustifyLastLine2(t *testing.T) {
+	exampleLine := "for your country"
+	assert.Equal(t, exampleLine, leftJustify([]string{"for","your","country"}, 16))
+}
+
 func TestJustifyCenter(t *testing.T) {
 	exampleLine := "is    a    form   of"
 
 	assert.Equal(t, exampleLine, justifyCenter([]string{"is", "a", "form", "of"}, 20))
+}
+
+func TestJustifyCenter2(t *testing.T) {
+	exampleLine := "what  you can do"
+
+	assert.Equal(t, exampleLine, justifyCenter([]string{"what", "you", "can", "do"}, 20))
 }
